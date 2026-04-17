321a6ca57d5a9ada39d86c50f6961dc35dbd84df
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-04-17 08:55:54 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.sql.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        String url ="jdbc:oracle:thin:@localhost:1521:xe";
+        String usuario = "RIBERA";
+        String contraseña = "ribera";
+
+        try(Connection conn = DriverManager.getConnection(url, usuario, contraseña);
+            Statement statement = conn.createStatement()){
+
+            String sql = "SELECT * FROM empleado WHERE MOD(ID,2) != 0 ";
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                int id= resultSet.getInt("id");
+                String nombre= resultSet.getString("nombre");
+                double salario= resultSet.getDouble("salario");
+                System.out.println("ID: " +id+ " Nombre: "+nombre+" Salario: "+salario);
+            }
+
+
+        }catch(SQLException e ){
+            System.out.println("Error al crear la tabla" + e.getMessage());
         }
+
     }
 }
